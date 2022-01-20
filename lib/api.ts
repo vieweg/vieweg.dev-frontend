675b053f01c63c4c1c6f82936198146302329982
Rafael Vieweg <vieweg@gmail.com>
2022-01-20 21:36:52 +0000

    added suport to multi environment

--- lib/api.ts
+++ lib/api.ts
@@ -15,6 +15,7 @@ import {
 export const client = createClient({
   space: process.env.CF_SPACE_ID || "",
   accessToken: process.env.CF_ACCESS_TOKEN || "",
+  environment: "dev",
 });
 
 const previewClient = createClient({
